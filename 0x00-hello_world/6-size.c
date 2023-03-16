799a1815a2861fa020f9979b19637a10a06c1f16
Pheenah <my>
2023-03-16 16:54:56 -0700

    size of bytes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+*main - this is a main function
+*Return: 0 on sucess
+*/
+int main(void)
+{
+printf("Size of a char:1%lu byte(s)", sizeof(char));
+printf("Size of int :%lu byte(s)", sizeof(int));
+printf("Size of a long int:%lu byte(s)", sizeof(long int));
+printf("Size of a long long int:%lu byte(s)", sizeof(long long int));
+printf("Size of a float:%lu byte(s)", sizeof(float));
+return (0);
+}
