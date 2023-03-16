af68fae45aac88b06d7ef534fa4ce7334f92f478
Pheenah <my>
2023-03-16 15:51:02 -0700

    piece of art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - this is the main function
+*Return: 0 when successful
+*/
+int main(void)
+{
+print(" with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
