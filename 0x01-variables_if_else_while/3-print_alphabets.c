f4526629ccab417e4ec03fad809c895bea3a499b
Pheenah <my>
2023-03-17 18:04:23 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,10 +6,15 @@
 */
 int main(void)
 {
-int n = 97;
-for (n = 90; n <= 122; n++)
+int n;
+int m;
+for (n = 97; n <= 122; n++)
 {
 putchar(n);
+}
+for (m = 65; m <= 90; m++)
+{
+putchar(m);
 }
 putchar('\n');
 return (0);
