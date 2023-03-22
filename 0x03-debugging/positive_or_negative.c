8b0e08f364bfad613d08b73ef9d485a0c96a1b4e
Pheenah <my>
2023-03-22 10:55:38 -0700

    testing positiveor negetive bug

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* positive_or_negetive - checks for positive or negativenumbers
+*
+* @i: the number to be checked
+* Return: always 0
+*/
+void positive_or_negative(int i)
+{
+if (i < 0)
+printf("%d is negetive\n", i);
+else
+printf("%d is positive\n", i);
+}
