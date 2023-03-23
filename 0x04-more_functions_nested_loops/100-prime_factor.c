2e5c2c9a0c2a9b4eb97a9e34b57c8fe3341dd468
Pheenah <my>
2023-03-23 14:18:28 -0700

    prime number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,8 +7,8 @@
 */
 
 int main(void)
-
 {
+
 long x, maxf;
 long number = 612852475143;
 double square = sqrt(number);
@@ -17,7 +17,7 @@ for (x = 1; x <= square; x++)
 {
 if (number % x == 0)
 {
-maxf = number /x;
+maxf = number / x;
 }
 }
 printf("%ld\n", maxf);
