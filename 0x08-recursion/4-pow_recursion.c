9b9dba8793080bce36ff60d4da76e7a3e4ea3cc4
Pheenah <my>
2023-04-05 06:40:45 -0700

    recursion power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,13 +4,16 @@
 * @x: the value to multiply
 * @y: the times to multiply the value
 *
+* Return: the value multiplied y times
 */
 
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
+
 	if (y == 0)
 		return (1);
-	return (x * _pow_recursion(x, y -1));
+
+	return (x * _pow_recursion(x, y - 1));
 }
