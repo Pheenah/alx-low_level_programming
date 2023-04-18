ec2d8ca9ea5873338ae330a927c49b19cd0d25bc
Pheenah <my>
2023-04-17 18:29:40 -0700

    newfile

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - prints name of file
+*
+* Return: always 0
+**/
+
+int main(void)
+{
+putchar ('F');
+putchar ('I');
+putchar ('L');
+putchar ('E');
+putchar ('\n');
+return (0);
+}
